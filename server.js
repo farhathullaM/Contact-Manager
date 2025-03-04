31cf3315d50ccad17a74f6b2ca562ce0cf80f512
farhathulla <farhathulla01@gmail.com>
2025-03-04 20:35:04 +0530

    db connection

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import contactRouter from "./routes/contactRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import connectDB from "./config/dbConnection.js";
 
 dotenv.config();
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(errorHandler)
 app.use("/api/contacts", contactRouter);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
